4472565ecd73118dc137901ac8138a98893e041e
HP <sriniwassg23@gmail.com>
2023-07-23 20:07:39 +0530

    frentend shell and config file

--- frentend.sh
+++ frentend.sh
@@ -5,5 +5,6 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-## nginxconfig file
+cp frentend.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
+
