9ca5fc8822699945b294e82000491e89186a70eb
HP <sriniwassg23@gmail.com>
2023-07-25 22:01:49 +0530

    adding all script

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,10 @@
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
+cp frontend.conf /etc/nginx/default.d/roboshop.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 systemctl restart nginx
+
