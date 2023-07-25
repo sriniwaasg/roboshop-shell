1c13931768f01ecf29e32175166bd86a9c1dbec6
HP <sriniwassg23@gmail.com>
2023-07-25 06:09:49 +0530

    adding all script

--- frentend.sh
+++ frentend.sh
@@ -1,12 +1,13 @@
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
-cp frentend.conf /etc/nginx/default.d/roboshop.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 systemctl restart nginx
+
+
 
 
 
