e28841afc7af9b65bc66e432ffa6e8ca43d91fdb
HP <sriniwassg23@gmail.com>
2023-07-26 06:55:24 +0530

    adding all script

--- mongo.sh
+++ mongo.sh
@@ -2,7 +2,9 @@ cp mongo.repos /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
-## chage ip 127.0.0.0 to 0.0.0.0
+sed -i -e 's|127.0.0.1|0.0.0.0|' etc/mongod.conf
 systemctl restart mongod
+
+
 
 
