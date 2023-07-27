d497e78cea32d9b173541266d0c099b7f67b69ed
HP <sriniwassg23@gmail.com>
2023-07-27 07:08:04 +0530

    catalogue script

--- cart.sh
+++ cart.sh
@@ -1,12 +1,21 @@
+echo -e "\e[36m>>>>>>>>>>install repo<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[36m>>>>>>>>>>install nodejs<<<<<<<<<\e[0m"
 yum install nodejs -y
+echo -e "\e[36m>>>>>>>>>>add user<<<<<<<<<\e[0m"
 useradd roboshop
+echo -e "\e[36m>>>>>>>>>>add app<<<<<<<<<\e[0m"
 mkdir /app
+echo -e "\e[36m>>>>>>>>>>download contat<<<<<<<<<\e[0m"
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+echo -e "\e[36m>>>>>>>>>>unzip contant<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/cart.zip
+echo -e "\e[36m>>>>>>>>>>install dependency<<<<<<<<<\e[0m"
 npm install
-cp cart.ser catalogue-dev.sriniwaasg23.online
+echo -e "\e[36m>>>>>>>>>>cart config<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/cart.ser catalogue-dev.sriniwaasg23.online
+echo -e "\e[36m>>>>>>>>>>start cart<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable cart
 systemctl start cart
