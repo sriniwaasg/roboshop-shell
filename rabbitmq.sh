f8caa2cc0f0ee482b71d42eee3ff1dde5f800140
HP <sriniwassg23@gmail.com>
2023-07-27 07:59:09 +0530

    catalogue script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,12 @@
+echo -e "\e[36m>>>>>>>>>>>>>install rabbitmq package<<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+echo -e "\e[36m>>>>>>>>>>>>>install rabbitmq<<<<<<<<<<<\e[0m"
 yum install rabbitmq-server -y
+echo -e "\e[36m>>>>>>>>>>>>>enable rabbitmq<<<<<<<<<<<\e[0m"
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
+echo -e "\e[36m>>>>>>>>>>>>>add user<<<<<<<<<<<\e[0m"
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
