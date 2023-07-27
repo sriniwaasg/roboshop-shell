02be293f75df6adeae5a75749016070fd107e769
HP <sriniwassg23@gmail.com>
2023-07-27 07:49:25 +0530

    catalogue script

--- shipping.sh
+++ shipping.sh
@@ -1,16 +1,28 @@
+echo -e "\e[36m>>>>>>>>>install maven<<<<<<<<<<<<\e[0m"
 yum install maven -y
+echo -e "\e[36m>>>>>>>>>add application user<<<<<<<<<<<<\e[0m"
 useradd roboshop
+echo -e "\e[36m>>>>>>>>>add application app<<<<<<<<<<<<\e[0m"
 mkdir /app
+echo -e "\e[36m>>>>>>>>>install application<<<<<<<<<<<<\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+echo -e "\e[36m>>>>>>>>>unzip application<<<<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/shipping.zip
+echo -e "\e[36m>>>>>>>>>clan maven<<<<<<<<<<<<\e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
-cp shipping.ser /etc/systemd/system/shipping.service
+echo -e "\e[36m>>>>>>>>>copy configuration<<<<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/shipping.ser /etc/systemd/system/shipping.service
+echo -e "\e[36m>>>>>>>>>start shipping<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable shipping
 systemctl start shipping
+echo -e "\e[36m>>>>>>>>>install mysql<<<<<<<<<<<<\e[0m"
 yum install mysql -y
+echo -e "\e[36m>>>>>>>>>add username pass<<<<<<<<<<<<\e[0m"
 mysql -h mysql-dev.sriniwaasg23.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+echo -e "\e[36m>>>>>>>>>restart<<<<<<<<<<<<\e[0m"
 systemctl restart shipping
+
 
