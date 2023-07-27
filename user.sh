78cfff7fb408a6d13fa139bd2d62084c656f6d59
HP <sriniwassg23@gmail.com>
2023-07-27 06:35:47 +0530

    catalogue script

--- user.sh
+++ user.sh
@@ -1,14 +1,26 @@
+echo -e "\e[36m>>>>>>>>>install repos<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[36m>>>>>>>>>>>install nodejs<<<<<<<\e[0m"
 yum install nodejs -y
+echo -e "\e[36m>>>>>>>>>>add user<<<<<<<<\e[0m"
 useradd roboshop
+echo -e "\e[36m>>>>>>>>>>add app<<<<<<<<\e[0m"
 mkdir /app
+echo -e "\e[36m>>>>>>>>>>down load app<<<<<<<<\e[0m"
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+echo -e "\e[36m>>>>>>>>>>>unzip nodejs<<<<<<\e[0m"
 cd /app
 unzip /tmp/user.zip
+echo -e "\e[36m>>>>>>>>>>>install repo<<<<<<\e[0m"
 npm install
-cp user.ser /etc/systemd/system/user.service
+echo -e "\e[36m>>>>>>>>>>>config setup<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/user.ser /etc/systemd/system/user.service
+echo -e "\e[36m>>>>>>>>>>>start user<<<<<<\e[0m"
 systemctl enable user
 systemctl start user
-cp mongo.repos /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m>>>>>>>>>>>install mongorepo<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/mongo.repos /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m>>>>>>>>>>>install mongo<<<<<<\e[0m"
 yum install mongodb-org-shell -y
+echo -e "\e[36m>>>>>>>>>>>Load schema<<<<<<\e[0m"
 mongo --host mongodb-dev.sriniwaasg23.online </app/schema/user.js
