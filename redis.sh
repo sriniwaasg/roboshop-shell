77d95b6e27e24f94a2d75e1b7ba573ea924030c3
HP <sriniwassg23@gmail.com>
2023-07-27 05:36:53 +0530

    catalogue script

--- redis.sh
+++ redis.sh
@@ -1,6 +1,12 @@
+echo -e "\e[36m>>>>>>>>>>install rpm<<<<<<<<<<\e[0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+echo -e "\e[36m>>>>>>>>>>enable redis<<<<<<<<\e[0m"
 yum module enable redis:remi-6.2 -y
+echo -e "\e[36m>>>>>>>>>>>install redis<<<<<<<<<\e[0m"
 yum install redis -y
-## edit ip address 127.0.0.1 with 0.0.0.0
+echo -e "\e[36m>>>>>>>>>>>edit ip <<<<<<<<<<\e[0m"
+sed 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+echo -e "\e[36m>>>>>>>>>>start redis<<<<<<<<<\e[0m"
 systemctl enable redis
 systemctl start redis
+
