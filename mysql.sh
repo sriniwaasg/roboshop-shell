da8a483ef579f1cf56c1f671fd0f63670898bdc8
HP <sriniwassg23@gmail.com>
2023-07-27 07:33:11 +0530

    catalogue script

--- mysql.sh
+++ mysql.sh
@@ -1,8 +1,13 @@
+echo -e "\e[36m>>>>>>>>>>>>disable module<<<<<<<<<\e[0m"
 yum module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "\e[36m>>>>>>>>>>>>copy msql repo<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "\e[36m>>>>>>>>>>>>install mysql<<<<<<<<<\e[0m"
 yum install mysql-community-server -y
+echo -e "\e[36m>>>>>>>>>>>>sart mysql<<<<<<<<<\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
+echo -e "\e[36m>>>>>>>>>>>>set user pass <<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
 
 
