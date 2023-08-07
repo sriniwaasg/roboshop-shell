45d02caf6ebeb851ba76a955e739c94ed11c2def
HP <sriniwassg23@gmail.com>
2023-08-07 20:14:30 +0530

    catalogue script

--- common.sh
+++ common.sh
@@ -2,50 +2,90 @@ app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 
-print_head() {
+func_print_head() {
   echo -e "\e[35m>>>>>>>>>> $1 <<<<<<<<<\e[0m"
 }
-schema_setup() {
+ func_schema_setup() {
   if [ "$schema_setup" == "mongo" ]; then
 
-  print_head "installmongorepo"
+  func_print_head "installmongorepo"
   cp ${script_path}/mongo.repos /etc/yum.repos.d/mongo.repo
 
-  print_head "install mongo"
+  func_print_head "install mongo"
   yum install mongodb-org-shell -y
 
-  print_head "Load schema"
+  func_print_head "Load schema"
   mongo --host mongodb-dev.sriniwaasg23.online </app/schema/${component}.js
   fi
+  if[ "$schema_setup" == "mysql"];then
+
+  func_print_head "install mysql"
+  yum install mysql -y
+
+  func_print_head "add username pass"
+  mysql -h mysql-dev.sriniwaasg23.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+ fi
 
 }
+func_app_prereq(){
+  func_print_head "add application user"
+  useradd roboshop
 
+ func_print_head "create application directory"
+ rm -rf /app
+ mkdir /app
+
+ func_print_head "download application contant"
+ curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+
+ func_print_head "extract application contant"
+ cd /app
+ unzip /tmp/${component}.zip
+
+}
+func_systemd_setup(){
+  func_print_head "copy configuration"
+  cp ${script_path}/${component}.ser /etc/systemd/system/${component}.service
+
+  func_print_head "start ${component}"
+  systemctl daemon-reload
+  systemctl enable ${component}
+  systemctl start ${component}
+  systemctl restart ${component}
+}
 func_nodejs() {
-print_head "install repo"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-print_head "install nodejs"
-yum install nodejs -y
-print_head "add user"
-useradd roboshop
-print_head "add app"
-rm -rf /app
-mkdir /app
-print_head "download contat"
-curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-print_head "unzip contant"
-cd /app
-unzip /tmp/${component}.zip
-print_head "install dependency"
-npm install
-print_head "cart config"
-cp ${script_path}/${component}.ser  /etc/systemd/system/${component}.service
-print_head "start cart"
-systemctl daemon-reload
-systemctl enable ${component}
-systemctl start ${component}
+ func_print_head "install repo"
+ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-schema_setup
+ func_print_head "install nodejs"
+ yum install nodejs -y
+
+ func_app_prereq
+
+ func_print_head "install dependency"
+ npm install
+
+ func_schema_setup
+ func_systemd_setup
+
+
 
 }
+func_java(){
+ func_print_head "install"
+ yum install maven -y
 
+ func_app_prereq
+
+ func_print_head "maven dependencies"
+ mvn clean package
+ mv target/${component}-1.0.jar ${component}.jar
+
+ func_schema_setup
+ func_systemd_setup
+
+
+
+
+}
 
--- shipping.sh
+++ shipping.sh
@@ -5,34 +5,10 @@ mysql_root_password=$1
 if [ -z "$mysql_root_password" ]; then
   echo input mysql root passwor missing
   exit
-  fi
-
-echo -e "\e[36m>>>>>>>>>install maven<<<<<<<<<<<<\e[0m"
-yum install maven -y
-echo -e "\e[36m>>>>>>>>>add application user<<<<<<<<<<<<\e[0m"
-useradd roboshop
-echo -e "\e[36m>>>>>>>>>add application app<<<<<<<<<<<<\e[0m"
-mkdir /app
-echo -e "\e[36m>>>>>>>>>install application<<<<<<<<<<<<\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-echo -e "\e[36m>>>>>>>>>unzip application<<<<<<<<<<<<\e[0m"
-cd /app
-unzip /tmp/shipping.zip
-echo -e "\e[36m>>>>>>>>>clan maven<<<<<<<<<<<<\e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-echo -e "\e[36m>>>>>>>>>copy configuration<<<<<<<<<<<<\e[0m"
-cp ${script_path}/shipping.ser /etc/systemd/system/shipping.service
-echo -e "\e[36m>>>>>>>>>start shipping<<<<<<<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl start shipping
-echo -e "\e[36m>>>>>>>>>install mysql<<<<<<<<<<<<\e[0m"
-yum install mysql -y
-echo -e "\e[36m>>>>>>>>>add username pass<<<<<<<<<<<<\e[0m"
-mysql -h mysql-dev.sriniwaasg23.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
-echo -e "\e[36m>>>>>>>>>restart<<<<<<<<<<<<\e[0m"
-systemctl restart shipping
+fi
+component="shipping"
+schema_setup=mysql
+func_java
 
 
 
