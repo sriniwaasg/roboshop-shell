f133e6551f8385359a4caa931b2f656fd4797dee
HP <sriniwassg23@gmail.com>
2023-08-09 05:23:35 +0530

    catalogue script

--- frontend.sh
+++ frontend.sh
@@ -2,13 +2,31 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
-cp frontend.conf /etc/nginx/default.d/roboshop.conf
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl restart nginx
+func_print_head "install nginx"
+yum install nginx -y &>>$log_file
+func_stat_check $?
+
+func_print_head "copy config file"
+cp frontend.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
+func_stat_check $?
+
+func_print_head "clean nginx"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+func_stat_check $?
+
+func_print_head "down load content"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+func_stat_check $?
+
+func_print_head "extract content"
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>$log_file
+func_stat_check $?
+
+func_print_head "start nginx"
+systemctl enable nginx &>>$log_file
+systemctl start nginx &>>$log_file
+systemctl restart nginx &>>$log_file
+func_stat_check $?
+
 
