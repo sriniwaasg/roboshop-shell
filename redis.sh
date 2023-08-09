f4c9ad3a5a1f525a3c5d9d85a44706d588acf88a
HP <sriniwassg23@gmail.com>
2023-08-09 07:14:56 +0530

    catalogue script

--- redis.sh
+++ redis.sh
@@ -1,13 +1,30 @@
-echo -e "\e[36m>>>>>>>>>>install rpm<<<<<<<<<<\e[0m"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-echo -e "\e[36m>>>>>>>>>>enable redis<<<<<<<<\e[0m"
-yum module enable redis:remi-6.2 -y
-echo -e "\e[36m>>>>>>>>>>>install redis<<<<<<<<<\e[0m"
-yum install redis -y
-echo -e "\e[36m>>>>>>>>>>>edit ip <<<<<<<<<<\e[0m"
-sed -i -e's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
-echo -e "\e[36m>>>>>>>>>>start redis<<<<<<<<<\e[0m"
-systemctl enable redis
-systemctl restart redis
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
+
+
+func_print_head "install rpm"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>log_file
+func_stat_check $?
+
+
+func_print_head "enable redis"
+yum module enable redis:remi-6.2 -y &>>log_file
+func_stat_check $?
+
+func_print_head "install redis"
+yum install redis -y &>>log_file
+func_stat_check $?
+
+func_print_head "edit ip"
+sed -i -e's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf &>>log_file
+func_stat_check $?
+
+func_print_head "start redis"
+systemctl enable redis &>>log_file
+systemctl restart redis &>>log_file
+func_stat_check $?
+
+
 
 
