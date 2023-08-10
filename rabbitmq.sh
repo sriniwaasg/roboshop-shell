f2df52ca59ae32ddd92b18630df4397da48cf272
HP <sriniwassg23@gmail.com>
2023-08-10 05:43:13 +0530

    catalogue script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -3,7 +3,7 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 rabbitmq_user_password=$1
 
-if [ -z "$rabbitmq_user_password"]; then
+if [ -z "$rabbitmq_user_password" ]; then
   echo input roboshop appuser password missing
   exit
   fi
