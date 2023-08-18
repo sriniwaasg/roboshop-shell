86dadaf53b9af285507a9e7bbd8d60ffe55b0b70
HP <sriniwassg23@gmail.com>
2023-08-18 20:19:27 +0530

    catalogue script

--- user.sh
+++ user.sh
@@ -4,46 +4,8 @@ source ${script_path}/common.sh
 
 component=user
 schema_setup=mongo
+func_nodejs
 
-func_print_head "download rmp"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
-func_print_head "install nodejs"
-yum install nodejs -y
-
-func_print_head "add application user"
-useradd roboshop
-
-func_print_head "add application directory"
-mkdir /app
-
-func_print_head "download app contant"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-
-func_print_head "extract app contant"
-cd /app
-unzip /tmp/user.zip
-
-func_print_head "install npm"
-cd /app
-npm install
-
-func_print_head "copy config file"
-cp user.ser /etc/systemd/system/user.service
-
-func_print_head "restart component"
-systemctl daemon-reload
-systemctl enable user
-systemctl start user
-
-"copy mongo"
-cp mongo.repos /etc/yum.repos.d/mongo.repo
-
-"install mongo"
-yum install mongodb-org-shell -y
-
-"ip addres change"
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/user.js
 
 
 
