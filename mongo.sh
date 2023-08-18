9f7cfff9517739e84d93aa0b2f4b1a44170b8178
HP <sriniwassg23@gmail.com>
2023-08-19 05:07:08 +0530

    catalogue script

--- mongo.sh
+++ mongo.sh
@@ -3,20 +3,21 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 func_print_head "copy mongo repo"
-cp mongo.repos /etc/yum.repos.d/mongo.repo
+cp mongo.repos /etc/yum.repos.d/mongo.repo &>>$log_file
+func_stat_check $?
 
 func_print_head "install mongo"
-yum install mongodb-org -y
-
-func_print_head "start mongo"
-systemctl enable mongod
-systemctl start mongod
+yum install mongodb-org -y &>>$log_file
+func_stat_check $?
 
 func_print_head "replace ip"
-sed -i -e's|127.0.0.1|0.0.0.0|'
+sed -i -e's|127.0.0.1|0.0.0.0|' &>>$log_file
+func_stat_check $?
 
 func_print_head "restart mongo"
-systemctl restart mongod
+systemctl enable mongod &>>$log_file
+systemctl restart mongod &>>$log_file
+func_stat_check $?
 
 
 
