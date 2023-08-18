1d761ed1df7abd3f20571fe6f90b6406c2ccb11b
HP <sriniwassg23@gmail.com>
2023-08-18 20:44:39 +0530

    catalogue script

--- frontend.sh
+++ frontend.sh
@@ -11,23 +11,23 @@ func_stat_check $?
 
 func_print_head "clean nginx"
 rm -rf /usr/share/nginx/html/* &>>$log_file
-func_stat_check $
+func_stat_check $?
 
 func_print_head "download app contant"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
-func_stat_check $
+func_stat_check $?
 
 func_print_head "extract app contant"
 cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip &>>$log_file
-func_stat_check $
+func_stat_check $?
 
 func_print_head "conpy config file"
 cp frontend.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
-func_stat_check $
+func_stat_check $?
 
 func_print_head "start nginx"
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
-func_stat_check $
+func_stat_check $?
 
