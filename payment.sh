fb4b0dd31c8ba14206347cd999489ed8004b9be5
HP <sriniwassg23@gmail.com>
2023-08-20 05:52:05 +0530

    catalogue script

--- payment.sh
+++ payment.sh
@@ -1,6 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+rabbitmq_appuser_password=$1
 
 if [ -z "$rabbitmq_appuser_password" ];then
   echo input rabbitmq appuser password missing
