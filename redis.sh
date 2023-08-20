59e533092f8f8fc2b143514da340fb4a1138286c
HP <sriniwassg23@gmail.com>
2023-08-20 16:23:00 +0530

    catalogue script

--- redis.sh
+++ redis.sh
@@ -7,16 +7,14 @@ func_print_head "install redis rpm"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$log_file
 func_stat_check $?
 
-func_print_head "install packege"
-yum module enable redis:remi-6.2 -y &>>$log_file
-func_stat_check $?
 
 func_print_head "install redis"
+dnf module enable redis:remi-6.2 -y &>>$log_file
 yum install redis -y &>>$log_file
 func_stat_check $?
 
 func_print_head "repalce ip"
-sed -i -e 's|127.0.0.1|0.0.0.0|'  /etc/redis.conf  /etc/redis/redis.conf &>>$log_file
+sed -i -e 's|127.0.0.1|0.0.0.0|'  /etc/redis.conf  /etc/redis/redis.conf  &>>$log_file
 func_stat_check $?
 
 func_print_head "restart redis"
